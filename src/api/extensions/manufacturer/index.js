4e8169d7d6e189bb659d2e7b419e3db6ce5382af
Peter Stuifzand <peter@stuifzand.eu>
2019-02-12 16:15:22 +0100

    Add slug

--- src/api/extensions/manufacturer/index.js
+++ src/api/extensions/manufacturer/index.js
@@ -7,8 +7,10 @@ module.exports = ({config, db}) => {
   manuApi.get('/manufacturers', (req, res) => {
     let result = {
       manufacturers: [
-        {id: 1, name: 'Test'},
-        {id: 2, name: 'Test 2'}
+        {id: 1, name: 'Test 1', slug: 'test-1'},
+        {id: 2, name: 'Test 2', slug: 'test-2'},
+        {id: 3, name: 'Test 3', slug: 'test-3'},
+        {id: 4, name: 'Test 4', slug: 'test-4'}
       ]
     };
     return apiStatus(res, result)
